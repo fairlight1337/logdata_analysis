37817d9161ff1e6d2a3012151fb99f71048df73f
Jan Winkler <jan.winkler.84@gmail.com>
2016-10-31 11:22:36 +0100

    Added JSON transforming subclass to `DesignatorReader` to be able to parse improper CRAM JSON

--- scripts/tools/DesignatorReader.py
+++ scripts/tools/DesignatorReader.py
@@ -31,6 +31,84 @@ class DesignatorReader:
         entries = []
         with open(strFile) as f:
             for line in f:
-                entries.append(json.loads(line))
+                transformed_line = self.transformLine(line)
+                the_json = json.loads(transformed_line)
+                transformed_json = self.transformJSON(the_json)
+                
+                entries.append(transformed_json)
         
         return entries
+    
+    def transformLine(self, line):
+        # Default: No transformation
+        return line
+    
+    def transformJSON(self, raw_json):
+        # Default: No transformation
+        return raw_json
+
+
+class TransformingDesignatorReader(DesignatorReader):
+    def transformLine(self, line):
+        old_offset = 0
+        offset = old_offset
+        transformed_line = ""
+        idx = 0
+        
+        while offset > -1:
+            offset = line.find('"" : ', offset)
+            
+            if offset > -1:
+                transformed_line += line[old_offset:offset]
+                old_offset = offset + 5
+                
+                transformed_line += '"' + str(idx) + '" : '
+                idx += 1
+                offset += 5
+            else:
+                transformed_line += line[old_offset:]
+        
+        return transformed_line
+    
+    def representsInt(self, s):
+        try: 
+            int(s)
+            return True
+        except ValueError:
+            return False
+
+    def transformJSON(self, raw_json):
+        if isinstance(raw_json, list):
+            clean_list = []
+            for item in raw_json:
+                clean_list.append(self.transformJSON(item))
+            
+            return clean_list
+        elif isinstance(raw_json, dict):
+            clean_dict = {}
+            keys = raw_json.keys()
+            keys.sort()
+            
+            all_numeric = True
+            for key in keys:
+                #print key
+                if not self.representsInt(key):
+                    all_numeric = False
+                    break
+            
+            if all_numeric: # This is actually a list
+                # Transform
+                clean_list = []
+                
+                for key in keys:
+                    clean_list.append(self.transformJSON(raw_json[key]))
+                #print clean_list
+                return clean_list
+            else:
+                clean_dict = {}
+                for key in keys:
+                    clean_dict[key] = self.transformJSON(raw_json[key])
+                
+                return clean_dict
+        
+        return raw_json
